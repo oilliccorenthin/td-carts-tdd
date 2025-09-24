e5126f44560a0d76720445518aea043829fdbc22
EUKao <corenthin.oillic44@gmail.com>
2025-09-24 10:49:18 +0200

    Iteration 2 : Modifier / supprimer article

--- src/Cart.ts
+++ src/Cart.ts
@@ -8,6 +8,25 @@ export class Cart {
 	add(item: CartItem) {
 		this.items.push(item);
 	}
+	update(item: CartItem) {
+		const index = this.items.findIndex((i) => i.product === item.product);
+		if (index !== -1) {
+			this.items[index] = item;
+		}
+	}
+	remove(item: CartItem) {
+		const index = this.items.findIndex((i) => i.product === item.product);
+		if (index !== -1) {
+			const currentQty = this.items[index].quantity;
+			const newQty = currentQty - item.quantity;
+
+			if (newQty > 0) {
+				this.items[index].quantity = newQty;
+			} else {
+				this.items.splice(index, 1);
+			}
+		}
+	}
 	getItems() {
 		return this.items;
 	}
--- tests/Cart.test.ts
+++ tests/Cart.test.ts
@@ -7,4 +7,18 @@ describe('Cart', () => {
 
 		expect(cart.getItems()).toEqual([{ product: 'pomme', quantity: 2 }]);
 	});
+	test("retrait d'un article avec remove()", () => {
+		const cart = new Cart();
+		cart.add({ product: 'pomme', quantity: 2 });
+		cart.remove({ product: 'pomme', quantity: 1 });
+
+		expect(cart.getItems()).toEqual([{ product: 'pomme', quantity: 1 }]);
+	});
+	test('mise à jour de la quantité avec update()', () => {
+		const cart = new Cart();
+		cart.add({ product: 'pomme', quantity: 2 });
+		cart.update({ product: 'pomme', quantity: 3 });
+
+		expect(cart.getItems()).toEqual([{ product: 'pomme', quantity: 3 }]);
+	});
 });
