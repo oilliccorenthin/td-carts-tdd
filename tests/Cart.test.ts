878a16f2f3769e61ea0ab2fe634584e0acc6fea0
EUKao <corenthin.oillic44@gmail.com>
2025-09-24 10:52:20 +0200

    Iteration 2.1 : Modifier / supprimer article + tests complementaire

--- tests/Cart.test.ts
+++ tests/Cart.test.ts
@@ -14,6 +14,22 @@ describe('Cart', () => {
 
 		expect(cart.getItems()).toEqual([{ product: 'pomme', quantity: 1 }]);
 	});
+	test('remove supprime complètement si quantité tombe à 0', () => {
+		const cart = new Cart();
+		cart.add({ product: 'banane', quantity: 2 });
+		cart.remove({ product: 'banane', quantity: 2 });
+
+		expect(cart.getItems()).toEqual([]);
+	});
+
+	test('remove supprime complètement si quantité devient négative', () => {
+		const cart = new Cart();
+		cart.add({ product: 'orange', quantity: 1 });
+		cart.remove({ product: 'orange', quantity: 5 });
+
+		expect(cart.getItems()).toEqual([]);
+	});
+
 	test('mise à jour de la quantité avec update()', () => {
 		const cart = new Cart();
 		cart.add({ product: 'pomme', quantity: 2 });
